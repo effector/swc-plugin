8db8580009ca8e13b04ab7da72ab1d442f3ebbbe
Valentin Semirulnik <v7rulnik@gmail.com>
2025-09-01 20:57:09 +0400

    Fix tests

--- tests/fixtures/nextjs/use_client/output.js
+++ tests/fixtures/nextjs/use_client/output.js
@@ -1,17 +1,28 @@
 "use client";
+import { withFactory as _effector$factory } from 'effector';
 import { reflect, variant } from "@effector/reflect";
 import { BreweryCard, Loader } from "#root/shared/ui";
 import { $breweryOfTheDay } from "./model";
-export const BreweryOfTheDay = variant({
-    if: $breweryOfTheDay.map((brewery)=>!!brewery),
-    then: reflect({
-        view: BreweryCard,
-        bind: {
-            name: $breweryOfTheDay.map((brewery)=>brewery?.name ?? "Unknown"),
-            brewery_type: $breweryOfTheDay.map((brewery)=>brewery?.brewery_type ?? "nano"),
-            website_url: $breweryOfTheDay.map((brewery)=>brewery?.website_url ?? null),
-            image: $breweryOfTheDay.map((brewery)=>brewery?.image ?? "")
-        }
-    }),
-    else: Loader
+export const BreweryOfTheDay = _effector$factory({
+    sid: "1k46jcja",
+    name: "BreweryOfTheDay",
+    method: "variant",
+    fn: ()=>variant({
+            if: $breweryOfTheDay.map((brewery)=>!!brewery),
+            then: _effector$factory({
+                sid: "5uxnyvvo",
+                name: "then",
+                method: "reflect",
+                fn: ()=>reflect({
+                        view: BreweryCard,
+                        bind: {
+                            name: $breweryOfTheDay.map((brewery)=>brewery?.name ?? "Unknown"),
+                            brewery_type: $breweryOfTheDay.map((brewery)=>brewery?.brewery_type ?? "nano"),
+                            website_url: $breweryOfTheDay.map((brewery)=>brewery?.website_url ?? null),
+                            image: $breweryOfTheDay.map((brewery)=>brewery?.image ?? "")
+                        }
+                    })
+            }),
+            else: Loader
+        })
 });
--- tests/fixtures/view/force_scope/reflect_only/output.js
+++ tests/fixtures/view/force_scope/reflect_only/output.js
@@ -1,15 +1,21 @@
+import { withFactory as _effector$factory } from 'effector';
 import { useUnit } from "effector-react";
 import { reflect } from "@effector/reflect";
 // --- reflect/enabled ---
-const Name = reflect({
-    view: Input,
-    bind: {
-        value: $name,
-        placeholder: "Name"
-    },
-    useUnitConfig: {
-        forceScope: true
-    }
+const Name = _effector$factory({
+    sid: "2qq0v05m",
+    name: "Name",
+    method: "reflect",
+    fn: ()=>reflect({
+            view: Input,
+            bind: {
+                value: $name,
+                placeholder: "Name"
+            },
+            useUnitConfig: {
+                forceScope: true
+            }
+        })
 });
 // --- useUnit/disabled ---
 useUnit($name);
