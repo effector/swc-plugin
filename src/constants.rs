92cef85fe1e50fd8b275ed6cd664fb94a3cf480d
Valentin Semirulnik <v7rulnik@gmail.com>
2025-09-01 20:56:52 +0400

    Add @effector/reflect to default factories

--- src/constants.rs
+++ src/constants.rs
@@ -40,7 +40,7 @@ pub enum EffectorMethod {
 
 pub(crate) struct Internal {
     pub tracked:   [&'static str; 10],
-    pub factories: [&'static str; 5],
+    pub factories: [&'static str; 8],
 }
 
 pub(crate) static INTERNAL: Internal = Internal::new();
@@ -72,6 +72,9 @@ impl Internal {
                 "effector-action",
                 "@farfetched/core",
                 "@withease/factories",
+                "@effector/reflect",
+                "@effector/reflect/ssr",
+                "@effector/reflect/scope",
             ],
         }
     }
